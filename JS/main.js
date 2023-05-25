701873e59f3e3864de67ac6ec16c8540c6a71416
luizhenriquee01 <130650716+luizhenriquee01@users.noreply.github.com>
2023-05-25 10:08:03 -0300

    modificando o CPF

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   $('#telefone').mask('(00) 00000-0000');
   $('#cep').mask('00000-000');
-  $('#cpf').mask('000-000-000-00');
+  $('#cpf').mask('000.000.000-00');
   $('#nomeCompleto').mask('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA', {
     translation: { A: { pattern: /[a-zA-Z\s]/ } },
   });
